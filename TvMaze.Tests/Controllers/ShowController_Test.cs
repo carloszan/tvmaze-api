b79eb954ee459758d55d3b5ffd4a09b8b5d5fdc2
Carlos <carloshlzansavio@gmail.com>
2022-09-23 21:06:32 +0100

    add GetNext250DocumentsByIdAsync_WithValidId_ReturnsShows test

---
+++ TvMaze.Tests/Controllers/ShowController_Test.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using TvMazeApi.Controllers;
+using TvMazeApi.Entities;
+using TvMazeApi.Repositories;
+
+namespace TvMaze.Tests.Controllers
+{
+  public class ShowControllerTest
+  {
+    [Test]
+    public async Task GetNext250DocumentsByIdAsync_WithValidId_ReturnsShows()
+    {
+      // Arrange
+      var loggerMock = new Mock<ILogger<ShowController>>();
+      var showRepositoryMock = new Mock<IShowRepository>();
+
+      var lastId = 1;
+      var fakeCast = new List<Actor> { new Actor { Id = 1, Name = "John Travolta", Birthday = new DateTime(1969, 1, 1) } };
+      var fakeShows = new List<ShowEntity>() { new ShowEntity { Id = lastId, Name = "Game of Thrones", Cast = fakeCast } };
+
+      showRepositoryMock
+        .Setup(x => x.GetNext250DocumentsByIdAsync(It.IsAny<int>()))
+        .ReturnsAsync(fakeShows);
+
+      var showController = new ShowController(
+        loggerMock.Object, 
+        showRepositoryMock.Object);
+
+      // Act
+      var shows = await showController.Get("0");
+
+      // Assert
+      Assert.That(shows.FirstOrDefault().Id, Is.EqualTo(lastId));
+    }
+  }
+}
